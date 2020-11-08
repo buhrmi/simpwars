df2efaf3710ad7897313474db7e4fe63fea1b0cd
Stefan Buhrmester <buhrmi@gmail.com>
2020-11-08 17:56:12 +0100

    fix migration

--- db/migrate/20201105161102_timestamps_to_ints.rb
+++ db/migrate/20201105161102_timestamps_to_ints.rb
@@ -1,6 +1,6 @@
 class TimestampsToInts < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :last_hp_updated_at, :integer, 'integer USING CAST(last_hp_updated_at AS integer)'
-    add_column :users, :last_attacked_at, :integer, 'integer USING CAST(last_attacked_at AS integer)'
+    change_column :users, :last_hp_updated_at, 'integer USING CAST(last_hp_updated_at AS integer)'
+    add_column :users, :last_attacked_at, 'integer USING CAST(last_attacked_at AS integer)'
   end
 end
