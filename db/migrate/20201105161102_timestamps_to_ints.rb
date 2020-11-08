98c9fde818282adc6b7d218224e414a661015d32
Stefan Buhrmester <buhrmi@gmail.com>
2020-11-08 18:39:45 +0100

    fix migration

--- db/migrate/20201105161102_timestamps_to_ints.rb
+++ db/migrate/20201105161102_timestamps_to_ints.rb
@@ -1,6 +1,6 @@
 class TimestampsToInts < ActiveRecord::Migration[6.0]
   def change
     change_column :users, :last_hp_updated_at, 'integer USING extract(epoch from last_hp_updated_at)', default: 0
-    add_column :users, :last_attacked_at, 'integer USING extract(epoch from last_attacked_at)', default: 0
+    add_column :users, :last_attacked_at, :integer, default: 0
   end
 end
